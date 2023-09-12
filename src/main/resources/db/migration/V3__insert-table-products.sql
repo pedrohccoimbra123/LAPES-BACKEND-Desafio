d1d431aac59dc3199bb3f57506930ff61aa792da
Pedro <pedro22070215@aluno.cesupa.br>
2023-09-12 20:43:14 -0300

    Data injection directly on the database

---
+++ src/main/resources/db/migration/V3__insert-table-products.sql
@@ -0,0 +1,14 @@
+INSERT INTO products (category, product_name, price, unitsold)
+VALUES
+    ('eletronico','caixa de som jbl',195.00,2),
+    ('eletronico','echodot',199.00,15),
+    ('eletronico','macbook',7954.00,1),
+    ('livro','Entenda Algoritmos',47.70,21),
+    ('livro','PythonFluente',0.99,17),
+    ('periferico','Mouse Logitech',31.50 ,16),
+    ('periferico','tecladoGamer',150.00,45),
+    ('suplementoAlimentar','creatina',120.00,15),
+    ('suplementoAlimentar','termogenico',11.00,25),
+    ('suplementoAlimentar','bcaa',40.00,85),
+    ('suplementoAlimentar','Whey',113.00,16),
+    ('suplementoAlimentar','pastaDeAmendoin',20.00,90);
