4af5bb6d8abbc497ed4bec004ef75e86adc7f859
Pedro <pedro22070215@aluno.cesupa.br>
2023-09-12 20:43:40 -0300

    DTO and Exceptions handlers

---
+++ src/main/java/com/example/demo/infra/RequestExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.demo.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestExceptionHandler{
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(){
+
+        return ResponseEntity.badRequest().body("Não encontrado");
+
+    }
+}
